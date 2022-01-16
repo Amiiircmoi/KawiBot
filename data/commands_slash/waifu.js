7a51ea06beae09f010aa694c4ae0dedf7d3603c1
Amir <amiiir.games@gmail.com>
2022-01-17 00:44:21 +0100

    Ajout Reddit package et débuts essais

--- data/commands_slash/waifu.js
+++ data/commands_slash/waifu.js
@@ -1,14 +1,24 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Reddit } = require('reddit');
+const { RedditUsername, RedditPassword, RedditId, RedditSecret } = require('../config.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('waifupic')
         .setDescription('Affiche une image de ta waifu !')
-        .addStringOption( option =>
-            option.setName('waifu')
+        .addStringOption(option =>
+            option
+                .setName('waifu')
                 .setDescription('Lorem Ipsum')
                 .setRequired(true)),
     async execute(interaction) {
         await interaction.reply(`ui`);
     },
 };
+
+const reddit = new Reddit({
+    username: RedditUsername,
+    password: RedditPassword,
+    appId: RedditId,
+    appSecret: RedditSecret
+})
