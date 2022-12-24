28b0163ec6841b26d341ac4c604749f21a7206e8
Amir <amiiir@gmail.com>
2022-12-24 17:43:14 +0100

    Ajout de l'event BeReal !

--- Kawi.js
+++ Kawi.js
@@ -3,7 +3,7 @@ const { Client, Intents, Collection } = require('discord.js');
 const { token } = require('./data/config.json');
 const commands_slash = require('./data/deploy-commands');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.DIRECT_MESSAGES] });
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./data/commands_slash').filter(file => file.endsWith('.js'));
@@ -14,7 +14,7 @@ for (const file of commandFiles) {
 }
 
 client.once('ready', () => {
-    console.log('Ready!');
+    console.log('Kawi!');
     commands_slash.reloadCommands();
 
     // Défini l'activité de Kawi
@@ -35,5 +35,17 @@ client.on('interactionCreate', async interaction => {
         await interaction.reply({ content: 'Il y a eu une erreur lors de l\'excécution de la commande !', ephemeral: true });
     }
 });
+
+const eventFiles = fs.readdirSync('./data/events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const filePath = './data/events/' + file;
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
 
 client.login(token);
---
+++ data/events/bereal.js
@@ -0,0 +1,108 @@
+const { MessageEmbed } = require('discord.js');
+const { beRealChannelId, beRealRoleId } = require('../config.json');
+const devMode = false;
+
+module.exports = {
+    name: 'ready',
+    once: true,
+    execute(client) {
+        beReal(client); // Start BeReal
+    },
+};
+
+// Main function
+async function beReal(client) {
+    var beRealChannel = client.channels.cache.get(beRealChannelId);
+
+    var beRealTime = getTomorowRandomTime();
+    console.log(`Ready! Next BeReal at ${beRealTime}`);
+
+    while (true) {
+        console.log('Waiting for BeReal...');
+
+        if (new Date() > beRealTime || devMode) {
+
+            beRealChannel.send({
+                embeds: [new MessageEmbed()
+                    .setTitle(`Hop Hop Hop ! C'est l'heure de BeReal !`)
+                    .setDescription(`Faites nous rêver ${beRealChannel.guild.roles.cache.get('690685924664672316')} !`)
+                    .setColor('#FFFFFF')
+                    .setTimestamp(new Date().toISOString())
+                    .setImage(`https://bere.al/images/logos/bereal-twitter.png`)
+                ],
+            });
+
+            beRealChannel.guild.members.fetch().then(members => {
+                members.forEach(member => {
+                    if (!member.isBot && member.roles.cache.has(beRealRoleId)) {
+                        let user = member.user;
+                        user.createDM()
+                            .then(dm => {
+
+                                dm.send({
+                                    embeds: [new MessageEmbed()
+                                        .setTitle(`Hop Hop Hop ! C'est l'heure de BeReal !`)
+                                        .setDescription(`Fais nous rêver même si t'es aux chiottes ! Tu as 5 minutes !`)
+                                        .setColor('#FFFFFF')
+                                        .setImage(`https://bere.al/images/logos/bereal-twitter.png`)
+                                    ],
+                                });
+
+                                const filter = m => m.author.id === user.id && m.attachments.size > 0;
+                                const collector = dm.createMessageCollector({ filter, time: 10_000, max: 1 });
+                                collector.on('collect', m => {
+                                    let embed = new MessageEmbed()
+                                        .setTitle(`BeReal de ${user.username}`)
+                                        .setDescription(`Voici le BeReal de ${user} !`)
+                                        .setColor('#B2FCAD')
+                                        .setImage(m.attachments.first().url);
+
+                                    if (m.attachments.size > 1) {
+                                        embed.setThumbnail(m.attachments.last().url);
+                                    }
+                                    beRealChannel.send({
+                                        embeds: [embed],
+                                    });
+                                });
+                                collector.on('end', collected => {
+                                    if (collected.size == 0) {
+                                        beRealChannel.send(`Ce nullos de ${user} n'a pas envoyé son BeReal ! Huez le bouuuuhhhh !`);
+                                    }
+                                });
+
+                            });
+                    }
+                })
+            });
+
+            beRealTime = getTomorowRandomTime();
+            console.log(`Next BeReal at ${beRealTime}`);
+        }
+
+        await sleep(60000);
+    }
+}
+
+function getTomorowRandomTime() {
+    var date = new Date();
+    date.setDate(date.getDate() + 1);
+
+    if (date.getDay() < 5) {
+        date.setHours(getRandomArbitrary(9, 22));
+    } else if (date.getDay() == 5) {
+        date.setHours(getRandomArbitrary(9, 24));
+    } else {
+        date.setHours(getRandomArbitrary(12, 24));
+    }
+    date.setMinutes(Math.floor(Math.random() * 60));
+    date.setSeconds(Math.floor(Math.random() * 60));
+    return date;
+}
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
